0567616a3754f085302a1ba4d5add29456039574
TimBait <justlookfortim@gmail.com>
2024-01-26 00:03:58 +0500

    up

--- users/models.py
+++ users/models.py
@@ -2,10 +2,10 @@ from django.conf import settings
 from django.db import models
 
 
-class Users(models.Model):
-    login = models.CharField(max_length=40)
+class User(models.Model):
+    login = models.CharField(max_length=40, unique=True)
     password = models.CharField(max_length=40)
-    is_deleted = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'users'
